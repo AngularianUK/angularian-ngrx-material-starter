873bba509546a51a6009e59d27ba569b8a3b31db
Ian Smith (Angularian Ltd) <ian.smith@angularian.co.uk>
2019-03-17 21:09:10 +0000

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ian-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'angularian-ngrx-material-starter';
+}
